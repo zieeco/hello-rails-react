030a5d9bd4b1590758aa58380542b652b03aa638
zieeco <zieecosam@gmail.com>
2022-08-19 19:40:26 +0100

    Remove unused code

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -2,12 +2,5 @@ class Api::V1::GreetingsController < ApplicationController
   def index
     @greetings = Greeting.all.sample.greeting
     render json: { greeting: @greetings }
-    #   render json: { :greetings => [
-    #     {
-    #       :name => 'you',
-    #       :me => 'them'
-    #     }
-    #   ]
-    # }.to_json
   end
 end
